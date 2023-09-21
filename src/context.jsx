04cbcdddd92430f45af5e3043a7ff66d7adc9ac0
Nikitha75 <nikithanikki474@gmail.com>
2023-09-21 23:28:48 +0530

    Add toggle functionality for dark theme in App Context

---
+++ src/context.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+  };
+
+  return (
+    <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
