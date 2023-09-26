90e6a37565980ceaef8272a44b3f8e3cb96b638c
Nikitha75 <nikithanikki474@gmail.com>
2023-09-26 23:22:47 +0530

    Add search form

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const SearchForm = () => {
   return (
-    <div>SearchForm</div>
-  )
-}
+    <section>
+      <h1 className="title">images</h1>
+      <form className="search-form">
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="cat"
+        />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </section>
+  );
+};
 
-export default SearchForm
+export default SearchForm;
