b20827d14d3fc882367208d86947f93cfbfbbbfa
Nikitha75 <nikithanikki474@gmail.com>
2023-09-26 23:27:20 +0530

    Add function handleSubmit

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 const SearchForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    console.log(searchValue);
+  };
+
   return (
     <section>
       <h1 className="title">images</h1>
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <input
           type="text"
           className="form-input search-input"
