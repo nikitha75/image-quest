a404f06701437fd3fd4d80e0e2249f0d30e06162
Nikitha75 <nikithanikki474@gmail.com>
2023-10-02 23:31:39 +0530

    Render images

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -13,8 +13,47 @@ const Gallery = () => {
     },
   });
 
-  console.log(response);
-  return <div>Gallery</div>;
+  if (response.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+
+  if (response.isError) {
+    return (
+      <section className="image-container">
+        <h4>There was an error...</h4>
+      </section>
+    );
+  }
+
+  const results = response.data.results;
+
+  if (results.length < 1) {
+    return (
+      <section className="image-container">
+        <h4>No results found...</h4>
+      </section>
+    );
+  }
+
+  return (
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.urls?.regular;
+        return (
+          <img
+            src={url}
+            key={item.id}
+            alt={item.alt_description}
+            className="img"
+          />
+        );
+      })}
+    </section>
+  );
 };
 
 export default Gallery;
