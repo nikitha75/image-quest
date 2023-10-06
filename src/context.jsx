553192a7e542dd00e04eda91c427c45ec80a398b
Nikitha75 <nikithanikki474@gmail.com>
2023-10-06 23:52:10 +0530

    Add getInitialDarkMode for dark mode

--- src/context.jsx
+++ src/context.jsx
@@ -1,9 +1,17 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const AppContext = createContext();
 
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    "(prefers-color-schema:dark"
+  ).matches;
+  return prefersDarkMode;
+};
+
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  // const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
   const [searchTerm, setSearchTerm] = useState("cat");
 
   const toggleDarkTheme = () => {
